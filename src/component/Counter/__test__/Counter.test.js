04c56bae68bc3dc099a60677e4f57270b8dfb47e
Felexonyango <felexonyango19@gmail.com>
2022-04-23 21:47:30 +0300

    destructuring

--- src/component/Counter/__test__/Counter.test.js
+++ src/component/Counter/__test__/Counter.test.js
@@ -3,10 +3,17 @@ import {render,screen} from '@testing-library/react'
 import Counter from "../Counter"
 import '@testing-library/jest-dom/extend-expect'
 
+const renderComponent = () => {
+    return render(<Counter/>);
+  };
+  
+beforeEach(() => {
+ const {getByTestId} =renderComponent()
+})
 
 test('header renders with correct text',()=>{
 
- const  {getByTestId}=render(<Counter/>);
+
  const headerEl= screen.getByTestId("header")
 
 expect(headerEl.textContent).toBe("counter")
@@ -14,7 +21,6 @@ expect(headerEl.textContent).toBe("counter")
 
 })
 test("counter intially start at 0",()=>{
-const  {getByTestId}=render(<Counter/>);
 
 const counterEl= screen.getByTestId("counter")
 
@@ -23,7 +29,7 @@ expect(counterEl.textContent).toBe("0")
 
 })
 test("add button renders with correct +",()=>{
-    const  {getByTestId}=render(<Counter/>);
+
     
     const addEl= screen.getByTestId("btn")
     
@@ -32,7 +38,7 @@ test("add button renders with correct +",()=>{
     
     })
     test("sub button renders with correct -",()=>{
-        const  {getByTestId}=render(<Counter/>);
+
         
         const subtractEl= screen.getByTestId("sub")
         

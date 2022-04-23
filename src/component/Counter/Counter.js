3077c36c8916d32d453c102fc07bac02d2e61e1b
Felexonyango <felexonyango19@gmail.com>
2022-04-23 21:37:09 +0300

    counter test

---
+++ src/component/Counter/Counter.js
@@ -0,0 +1,20 @@
+import React,{useState} from 'react'
+
+const Counter = () => {
+    const [count,setCount] =useState(0)
+
+ 
+
+  return (
+    <div>
+      <h3 data-testid="header">counter</h3>
+    <p data-testid="counter">{count}</p> 
+  
+    
+<button onClick={()=>setCount(count+1)} data-testid='btn'>+</button>
+<button onClick={()=>setCount(count-1)} data-testid='sub'>-</button>
+    </div>
+  )
+}
+
+export default Counter
